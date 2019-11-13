f13c85d504fa536205f6e40c4c69b4a3f427f00b
Pinar Olguc <pinarolguc@gmail.com>
2019-11-13 14:10:52 +0300

    Update PORT config

--- server.rb
+++ server.rb
@@ -8,7 +8,7 @@ require 'time'        # Gets ISO 8601 representation of a Time object
 require 'logger'      # Logs debug statements
 require "base64"
 
-set :port, 3000
+set :port, ENV['PORT'].to_i
 set :bind, '0.0.0.0'
 
 
