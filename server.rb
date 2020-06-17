6ff066b1dfdb1621177956b44d7e26447dda463f
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-06-17 18:59:08 +0300

    Look for a bot config in target repo

--- server.rb
+++ server.rb
@@ -11,7 +11,6 @@ require "base64"
 set :port, ENV['PORT']
 set :bind, '0.0.0.0'
 
-
 # This is template code to create a GitHub App server.
 # You can read more about GitHub Apps here: # https://developer.github.com/apps/
 #
@@ -30,7 +29,6 @@ set :bind, '0.0.0.0'
 #
 
 class GHAapp < Sinatra::Application
-
   # Expects that the private key in PEM format. Converts the newlines
   PRIVATE_KEY = ( ENV['GITHUB_PRIVATE_KEY'] && !ENV['GITHUB_PRIVATE_KEY'].empty? ) ? OpenSSL::PKey::RSA.new(ENV['GITHUB_PRIVATE_KEY'].gsub('\n', "\n")) : ""
 
@@ -41,10 +39,9 @@ class GHAapp < Sinatra::Application
   # The GitHub App's identifier (type integer) set when registering an app.
   APP_IDENTIFIER = ENV['GITHUB_APP_IDENTIFIER']
 
+  # Default configuration
   TESTS_DIR = 'test-suites/'
-
   TESTS_MAPPING_FILE = 'config/mapping.json' 
-
   TESTS_REPO = 'wordpress-mobile/test-cases'
 
   # Turn on Sinatra's verbose logging during development
@@ -67,17 +64,15 @@ class GHAapp < Sinatra::Application
 
 
   post '/event_handler' do
-
-	case request.env['HTTP_X_GITHUB_EVENT']
-
-  when 'pull_request'
-    if @payload['action'] === 'opened'
-      handle_pullrequest_opened_event(@payload)
+    case request.env['HTTP_X_GITHUB_EVENT']
+      when 'pull_request'
+        if @payload['action'] === 'opened'
+          handle_pullrequest_opened_event(@payload)
+        end
+      #   if @payload['action'] === 'edited'
+      #     handle_pullrequest_opened_event(@payload)
+      # end
     end
-  #  if @payload['action'] === 'edited'
-  #    handle_pullrequest_opened_event(@payload)
-  #  end
-  end
 
     200 # success status
   end
@@ -92,41 +87,71 @@ class GHAapp < Sinatra::Application
     def handle_pullrequest_opened_event(payload)
       repo = payload['repository']['full_name']
       pr_number = payload['pull_request']['number']
-
+  
       logger.debug('A PR was opened in repo: ' + repo.to_s + ', PR number: ' + pr_number.to_s)
-
+  
+      fetch_config(repo, logger)
       mapping_json = fetch_mapping_json()
       matched_files = find_matched_testcase_files_from_pr(repo, pr_number, mapping_json)
       test_content = create_test_content(matched_files)
-
+  
       if matched_files.count > 0
         create_pull_request_review(repo, pr_number, test_content)
       end
     end
-
+  
     def fetch_mapping_json()
-      mapping = Octokit.contents(TESTS_REPO, :path => TESTS_MAPPING_FILE)
+      mapping = Octokit.contents(@config[:tests_repo], :path => @config[:mapping_file])
       content = mapping['content']
       plain = Base64.decode64(content)
       json = JSON.parse(plain)
     end
-
+  
+    def default_config()
+      {
+        tests_dir: 'test-suites/',
+        mapping_file: 'config/mapping.json',
+        tests_repo: 'wordpress-mobile/test-cases'
+      }
+    end
+  
+    def fetch_config(repo_name, logger)
+      config_file = '.github/test-case-reminder.json'
+      @config = default_config
+      
+      begin
+        response = Octokit.contents(repo_name, :path => config_file)
+      rescue => exception
+        logger.debug('No config file found. Falling back to default values')
+        return;
+      end
+  
+      content = response[:content]
+      plain = Base64.decode64(content)
+      json = JSON.parse(plain, {symbolize_names: true})
+      logger.debug("Found a config file for #{ repo_name }: #{ json }")
+  
+      @config.each do |key, value|
+        @config[key] = json[key] if ( json[key] )
+      end
+    end
+  
     def create_pull_request_review(repo, pr_number, test_content)
       options = { event: 'COMMENT', body: test_content }
       @installation_client.create_pull_request_review(repo, pr_number, options)
       logger.debug('Created pull request review:')
       logger.debug(test_content)
     end
-
+  
     def find_matched_testcase_files_from_pr(repo, pr_number, mapping_json)
       files = @installation_client.pull_request_files(repo, pr_number)
       matched_files = find_matched_testcase_files(files, mapping_json, logger)
     end
-
+  
     def find_matched_testcase_files(files, mapping_json, logger)
       logger.debug('Starting to run regex phrases on file names from the PR.')
       matched_files = []
-      files.each { |file| puts 
+      files.each { |file| puts
         filename = file['filename']
         logger.debug('filename: ' + filename.to_s)
         mapping_json.each { |item| puts
@@ -141,16 +166,16 @@ class GHAapp < Sinatra::Application
       }
       matched_files = matched_files.flatten.uniq
     end
-
+  
     def create_test_content(matched_files) 
       testContent = "Here are some suggested test cases for this PR. \n\n"
       matched_files.each { |file| puts
-            testFile = Octokit.contents(TESTS_REPO, :path => TESTS_DIR + file)
+            testFile = Octokit.contents(@config[:tests_repo], :path => @config[:tests_dir] + file)
             testContent = testContent + Base64.decode64(testFile['content'])
       }
       testContent = testContent + "\n\n" + " If you think that suggestions should be improved please edit the configuration file [here](https://github.com/wordpress-mobile/test-cases/blob/master/config/mapping.json). You can also modify/add [test-suites](https://github.com/wordpress-mobile/test-cases/tree/master/test-suites) to be used in the [configuration](https://github.com/wordpress-mobile/test-cases/blob/master/config/mapping.json).\n\n If you are a beginner in mobile platforms follow [build instructions](https://github.com/wordpress-mobile/test-cases/blob/master/README.md#build-instructions)."
     end
-
+  
     # Saves the raw payload and converts the payload to JSON format
     def get_payload_request(request)
       # request.body is an IO or StringIO object
@@ -164,7 +189,7 @@ class GHAapp < Sinatra::Application
         fail  "Invalid JSON (#{e}): #{@payload_raw}"
       end
     end
-
+  
     # Instantiate an Octokit client authenticated as a GitHub App.
     # GitHub App authentication requires that you construct a
     # JWT (https://jwt.io/introduction/) signed with the app's private key,
@@ -174,21 +199,21 @@ class GHAapp < Sinatra::Application
       payload = {
           # The time that this JWT was issued, _i.e._ now.
           iat: Time.now.to_i,
-
+  
           # JWT expiration time (10 minute maximum)
           exp: Time.now.to_i + (10 * 60),
-
+  
           # Your GitHub App's identifier number
           iss: APP_IDENTIFIER
       }
-
+  
       # Cryptographically sign the JWT.
       jwt = JWT.encode(payload, PRIVATE_KEY, 'RS256')
-
+  
       # Create the Octokit client, using the JWT as the auth token.
       @app_client ||= Octokit::Client.new(bearer_token: jwt)
     end
-
+  
     # Instantiate an Octokit client, authenticated as an installation of a
     # GitHub App, to run API operations.
     def authenticate_installation(payload)
@@ -196,7 +221,7 @@ class GHAapp < Sinatra::Application
       @installation_token = @app_client.create_app_installation_access_token(@installation_id)[:token]
       @installation_client = Octokit::Client.new(bearer_token: @installation_token)
     end
-
+  
     # Check X-Hub-Signature to confirm that this webhook was generated by
     # GitHub, and not a malicious third party.
     #
@@ -213,7 +238,7 @@ class GHAapp < Sinatra::Application
       method, their_digest = their_signature_header.split('=')
       our_digest = OpenSSL::HMAC.hexdigest(method, WEBHOOK_SECRET, @payload_raw)
       halt 401 unless their_digest == our_digest
-
+  
       # The X-GITHUB-EVENT header provides the name of the event.
       # The action value indicates the which action triggered the event.
       logger.debug "---- received event #{request.env['HTTP_X_GITHUB_EVENT']}"
